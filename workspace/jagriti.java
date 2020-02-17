1c833651dac4cea86c85d5fbcd2393c9d85b7b99
jagriti10 <jagriti.10sharma@gmail.com>
2020-02-17 13:05:25 +0530

    Comit by jagriti

---
+++ workspace/jagriti.java
@@ -0,0 +1,16 @@
+public static void main (String args[]) {
+2: Motorcycle m = new Motorcycle();
+3: m.make = “Yamaha RZ350”;
+4: m.color = “yellow”;
+5: System.out.println(“Calling showAtts...”);
+6: m.showAtts();
+7: System.out.println(“--------”);
+8: System.out.println(“Starting engine...”);
+9: m.startEngine();
+10: System.out.println(“--------”);
+11: System.out.println(“Calling showAtts...”);
+12: m.showAtts();
+13: System.out.println(“--------”);
+14: System.out.println(“Starting engine...”);
+15: m.startEngine();
+16:}
